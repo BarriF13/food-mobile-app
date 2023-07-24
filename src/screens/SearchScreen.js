f9e68bdf93c171d03cebac85b7d6bbfe144a327e
BarriF <barrifarjadnia@hollandandbarrett.com>
2023-07-24 15:44:45 +0100

    feat: add app js and first screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen =()=>{
+  return <View style={styles.searchScreen}>
+    <Text>Search Screen</Text>
+    </View>
+};
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
