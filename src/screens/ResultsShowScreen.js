f5a4043f0428ef894ca1115891814e15d1b9be9b
BarriF <barrifarjadnia@hollandandbarrett.com>
2023-12-15 13:31:38 +0000

    add yelp api to get single id

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,29 @@
-import React, { useState} from 'react';
+import React, { useState ,useEffect} from 'react';
 import { View, Text, StyleSheet,ScrollView  } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({navigation}) => {
+
+const [result, setResult] = useState(null);
+const id = navigation.getParam('id');
+console.log(result);
+ const getResult = async (id)=>{
+  
+  try {
+    const response = await yelp.get(`${id}`);
+    setResult(response.data);
+  }
+  catch(e){
+    setErrorMessage('Something went wrong');
+  }
+
+ };
+ useEffect(() => {
+  getResult(id);
+}, []);
+
   return <View>
-    <Text>Resss</Text>
+    <Text>result</Text>
   </View>
 }
 const styles = StyleSheet.create({});
